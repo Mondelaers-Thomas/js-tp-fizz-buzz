afd238b97be91d23b664ac44de893ff94c8912b2
Mondelaers Thomas <mondelaers.thomas@gmail.com>
2023-11-22 13:09:19 +0100

    :white_check_mark: finish commite

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+const FizzBuzz = {
+    init(){
+        document.body.classList.remove('no-js');
+        this.mOl = document.getElementById('fizzbuzz');
+        this.methode();
+    },
+    methode(){
+        for(let i = 1; i < 100; i++){
+            if(i % 15 === 0){
+                this.mOl.insertAdjacentHTML("beforeend", '<li class="fizzbuzz">FI<i>zz</i>BU<i>zz</i></li>');
+            } else if(i % 3 === 0){
+                this.mOl.insertAdjacentHTML("beforeend", '<li class="fizz">FI<i>zz</i></li>');
+            } else if(i % 5 === 0){
+                this.mOl.insertAdjacentHTML("beforeend", '<li class="buzz">BU<i>zz</i></li>');
+            } else {
+                this.mOl.insertAdjacentHTML("beforeend", `<li class="buzz">${i}</li>`);
+            }
+        }
+    }
+}
+FizzBuzz.init();
